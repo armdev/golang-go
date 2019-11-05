0e263003155e812466be4eb0b9a57c34911f93bc
Armen Arzumanyan <armena@YLT190013.corp.optym.net>
2019-11-05 18:28:29 +0400

    Refactoring

--- 2/values.go
+++ 2/values.go
@@ -10,4 +10,27 @@ func main() {
 	fmt.Println("1+1")
 	fmt.Println(!true)
 
+	var a = "init"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	
+	fmt.Println(d)
+
+	var e int
+
+	fmt.Println(e)
+
+	f := "apple"
+
+	fmt.Println(f)
+
+
+
+
+
+
 }
