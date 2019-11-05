253a95079c653e03bb01f87fdb4ea25bff47727b
Armen Arzumanyan <armena@YLT190013.corp.optym.net>
2019-11-05 18:11:41 +0400

    Refactoring

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # golang-go
+# install go
+# https://gobyexample.com/
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt-get install golang-go
+
---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("I am working!!!");
+	fmt.Println("Run me with: go run hello.go");
+	fmt.Println("Build me with: go build hello.go , and run like /hello");
+}
